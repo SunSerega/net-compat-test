f4d917abdd44a9b32f46191abbe2c2614e9e2737
Sun Serega <sunserega2@gmail.com>
2023-12-16 13:26:18 +0200

    run the .exe in docker

--- 0.pas
+++ 0.pas
@@ -1,7 +1,7 @@
 ﻿##
 
 WriteLines('otp.txt'
-  , typeof(IEnumerable<byte>).Assembly.GetTypes.Select(t->$'{t}')
+  , typeof(IEnumerable<byte>).Assembly.GetTypes.Select(t->$'{t}').PrintLines
   , new System.Text.UTF8Encoding(true)
 );
 
