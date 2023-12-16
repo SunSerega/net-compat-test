aa3ba13db13e6fa7ccfd64c988c7d35c24bb31a5
Sun Serega <sunserega2@gmail.com>
2023-12-16 16:45:29 +0200

    write versions

--- 0.pas
+++ 0.pas
@@ -1,8 +1,19 @@
 ﻿##
-
 WriteLines('otp.txt'
-  , typeof(IEnumerable<byte>).Assembly.GetTypes.Where(t->t.IsPublic).Select(t->$'{t}').PrintLines
+  , typeof(IEnumerable<byte>).Assembly.GetTypes.Where(t->t.IsPublic).Select(t->$'{t}')//.PrintLines
   , new System.Text.UTF8Encoding(true)
 );
+
+$'Assembly.Location: {typeof(object).Assembly.Location}'.Println;
+Println;
+$'Environment.Version: {System.Environment.Version}'.Println;
+Println;
+$'System.Runtime.InteropServices.RuntimeInformation.'.Println;
+$'FrameworkDescription: {System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription}'.Println;
+$'OSArchitecture:       {System.Runtime.InteropServices.RuntimeInformation.OSArchitecture}'.Println;
+$'OSDescription:        {System.Runtime.InteropServices.RuntimeInformation.OSDescription}'.Println;
+$'ProcessArchitecture:  {System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture}'.Println;
+
+
 
 ;
